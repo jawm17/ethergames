0aa3ac281b54c9e44136eb0660ee48329beb7e00
Michael Sanderson <jawm17@gmail.com>
2021-01-13 23:00:30 -0600

    account page

--- client/src/pages/Account/account.js
+++ client/src/pages/Account/account.js
@@ -1,16 +1,40 @@
 import React from "react";
-
+import history from "../../history";
+import "./accountStyle.css";
 
 export default function Account() {
 
+    function openCity(evt, tab) {
+        // Declare all variables
+        var i, tabcontent1, tabcontent2, tablinks;
+    
+        // Get all elements with class="tabcontent" and hide them
+        tabcontent1 = document.getElementsByClassName('tabcontent1');
+        for (i = 0; i < tabcontent1.length; i++) {
+            tabcontent1[i].style.display = 'none';
+        }
+        tabcontent2 = document.getElementsByClassName('tabcontent2');
+        for (i = 0; i < tabcontent2.length; i++) {
+            tabcontent2[i].style.display = 'none';
+        }
+    
+        // Get all elements with class="tablinks" and remove the class "active"
+        tablinks = document.getElementsByClassName('tablinks');
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(' active', '');
+        }
+    
+        // Show the current tab, and add an "active" class to the button that opened the tab
+        document.getElementById(tab).style.display = 'block';
+        evt.currentTarget.className += ' active';
+    }
+    
+
     return (
-        <div>
+        <div id="accountArea">
             <header>
                 <nav id="navAccount">
-                    <h1 id="logo"><a href="/">Crypto Arcade</a></h1>
-                    <ul className="nav-links">
-                        <li><a href="settings.html">Settings</a></li>
-                    </ul>
+                    <h1 id="accountLogo" onClick={() => history.push("/")}>Crypto Arcade</h1>
                 </nav>
             </header>
             <section className="containerAccount">
@@ -19,7 +43,7 @@ export default function Account() {
                         <h1>Username</h1>
                     </div>
                     <div className="QR-code">
-                        <img src="qr-code.png" alt="" />
+                        <img src="https://www.kaspersky.com/content/en-global/images/repository/isc/2020/9910/a-guide-to-qr-codes-and-how-to-scan-qr-codes-2.png" alt="" />
                     </div>
                     <div className="btn">
                         <button>Send</button>
@@ -30,10 +54,10 @@ export default function Account() {
                     {/* Tab links */}
                     <div className="tab-container">
                         <div className="tab">
-                            <button className="tablinks" onclick="openCity(event, 'Scores')">
+                            <button className="tablinks" onClick={(e) => openCity(e, 'Scores')}>
                                 Scores
                 </button>
-                            <button className="tablinks" onclick="openCity(event, 'Transactions')">
+                            <button className="tablinks" onClick={(e) => openCity(e, 'Transactions')}>
                                 Transactions
                 </button>
                         </div>
