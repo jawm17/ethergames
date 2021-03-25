7240a245eb1cb1b3299fc51534631fb815027be0
jonesliddy77 <jonesliddy77@gmail.com>
2021-03-24 22:07:48 -0500

    adding about page and editing home page

--- client/src/components/Nav/NavBar.js
+++ client/src/components/Nav/NavBar.js
@@ -1,69 +1,80 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 import history from "../../history";
-import AuthService from '../../services/AuthService';
-import { AuthContext } from '../../context/AuthContext';
+import AuthService from "../../services/AuthService";
+import { AuthContext } from "../../context/AuthContext";
 import "./navBarStyle.css";
 
+const NavBar = (props) => {
+  const { isAuthenticated, user, setIsAuthenticated, setUser } = useContext(
+    AuthContext
+  );
 
-const NavBar = props => {
-    const { isAuthenticated, user, setIsAuthenticated, setUser } = useContext(AuthContext);
+  const onClickLogoutHandler = () => {
+    AuthService.logout().then((data) => {
+      if (data.success) {
+        setUser(data.user);
+        setIsAuthenticated(false);
+      }
+    });
+  };
 
-    const onClickLogoutHandler = () => {
-        AuthService.logout().then(data => {
-            if (data.success) {
-                setUser(data.user);
-                setIsAuthenticated(false);
-            }
-        });
-    }
-
-    const unauthenticatedNavBar = () => {
-        return (
-            <>
-                <nav id="nav">
-                    <h1 id="logoMain" onClick={() => history.push("/")}>Ether Games</h1>
-                    <div className="nav-links">
-                        <div onClick={() => history.push("/")}>Arcade</div>
-                        <div onClick={() => history.push("/login")}>Log In</div>
-                    </div>
-                </nav>
-            </>
-        )
-    }
-
-    const authenticatedNavBar = () => {
-        if(props.page === "account") {
-            return (
-                <>
-                    <nav id="nav">
-                        <h1 id="logoMain" onClick={() => history.push("/")}>Ether Games</h1>
-                        <div className="nav-links">
-                            <div onClick={() => history.push("/settings")}>Settings</div>
-                            <div onClick={() => history.push("/logout")}>Log Out</div>
-                        </div>
-                    </nav>
-                </>
-            )
-        } else {
-            return (
-                <>
-                    <nav id="nav">
-                        <h1 id="logoMain" onClick={() => history.push("/")}>Ether Games</h1>
-                        <div className="nav-links">
-                            <div onClick={() => history.push("/")}>Arcade</div>
-                            <div onClick={() => history.push("/account")}>Wallet</div>
-                        </div>
-                    </nav>
-                </>
-            )
-        }
-    }
-  
+  const unauthenticatedNavBar = () => {
     return (
-        <nav>
-            {!isAuthenticated ? unauthenticatedNavBar() : authenticatedNavBar()}
+      <>
+        <nav id="nav">
+          <h1 id="logoMain" onClick={() => history.push("/")}>
+            Ether Games
+          </h1>
+          <div className="nav-links">
+            <div onClick={() => history.push("/about")}>About</div>
+            <div onClick={() => history.push("/")}>Arcade</div>
+            <div onClick={() => history.push("/login")}>Log In</div>
+          </div>
         </nav>
-    )
-}
+      </>
+      
+    );
+  };
+
+  const authenticatedNavBar = () => {
+    if (props.page === "account") {
+      return (
+        <>
+          <nav id="nav">
+            <h1 id="logoMain" onClick={() => history.push("/")}>
+              Ether Games
+            </h1>
+            <div className="nav-links">
+              <div onClick={() => history.push("/about")}>About</div>
+              <div onClick={() => history.push("/settings")}>Settings</div>
+              <div onClick={() => history.push("/logout")}>Log Out</div>
+            </div>
+          </nav>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <nav id="nav">
+            <h1 id="logoMain" onClick={() => history.push("/")}>
+              Ether Games
+            </h1>
+            <div className="nav-links">
+              <div onClick={() => history.push("/about")}>About</div>
+              <div onClick={() => history.push("/")}>Arcade</div>
+              <div onClick={() => history.push("/account")}>Wallet</div>
+            </div>
+          </nav>
+        </>
+      );
+    }
+  };
+
+  return (
+    <nav>
+      {!isAuthenticated ? unauthenticatedNavBar() : authenticatedNavBar()}
+    </nav>
+  );
+};
 
 export default NavBar;
---
+++ client/src/pages/About/AboutPage.js
@@ -0,0 +1,44 @@
+import React from "react";
+import NavBar from "../../components/Nav/NavBar";
+import Footer from "../../components/Footer/footer";
+import "./AboutPage.css";
+
+export default function About() {
+  return (
+    <div>
+      <NavBar />
+      <div id="about">
+        <div className="about-hero-container">
+            <div className="ether-logo"></div>
+        </div>
+      </div>
+      <div className="about-words-container">
+        <div className="about-words">
+          <div className="about-header">
+            <h1>About</h1>
+          </div>
+          <div className="about-paragraph">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Dignissim cras tincidunt lobortis feugiat vivamus at augue. Morbi
+              quis commodo odio aenean sed adipiscing diam donec.
+            </p>
+          </div>
+          <div className="about-header">
+            <h1>Future Development</h1>
+          </div>
+          <div className="about-paragraph">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Dignissim cras tincidunt lobortis feugiat vivamus at augue. Morbi
+              quis commodo odio aenean sed adipiscing diam donec.
+            </p>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
