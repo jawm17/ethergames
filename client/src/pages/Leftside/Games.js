7f12b6a90cbdcbcfc4402adb3d8239ef1c5c5c71
jonesliddy77 <jonesliddy77@gmail.com>
2021-06-06 23:34:13 -0700

    finished new layout

--- client/src/pages/Leftside/Games.js
+++ client/src/pages/Leftside/Games.js
@@ -2,21 +2,9 @@ import React, { useEffect, useState } from "react";
 import "./games.css";
 
 export default function GamesDiv() {
-  const DisplayFlex = {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center"
-  };
-  const DisplayFlex2 = {
-    display: "flex",
-    justifyContent: "space-around",
-    alignItems: "center"
-  };
   return (
-    <div  style={DisplayFlex} id="gamesArea">
-      <div style={DisplayFlex} id="gamesOuter-container">
-        <div style={DisplayFlex} id="gamesInner-container">
-          <div style={DisplayFlex2} id="games-container">
+    <div id="gamesArea">
+          <div id="games-container">
             <div id="snakeGame" className="games">
               <h1>Snake</h1>
             </div>
@@ -24,13 +12,11 @@ export default function GamesDiv() {
               <h1>Tetris</h1>
             </div>
             <div id="asteroidsGame" className="games">
-              <h1>Asteroids gmae</h1>
+              <h1>Asteroids</h1>
             </div>
             <div id="pacboyGame" className="games">
               <h1>PacBoy</h1>
             </div>
-          </div>
-        </div>
       </div>
     </div>
   );
--- client/src/pages/ThatNewNew/ThatNewNew.js
+++ client/src/pages/ThatNewNew/ThatNewNew.js
@@ -9,16 +9,48 @@ export default function ThatNewNew() {
     <div id="ThatNewNewPage">
       <div id="leftsideDiv">
         <div id="theGamesDiv">
-          <GamesDiv />
+          <div id="inner-theGamesDiv">
+            <div className="gameShadowDivTop"></div>
+            <div id="theGamesDiv-background1">
+              <div id="theGamesDiv-background2">
+                <div id="theGamesDiv-background3">
+                  <div id="theGamesDiv-background4">
+                    <div id="theGamesDiv-background5">
+                      <GamesDiv />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="gameShadowDiv"></div>
         </div>
         <div id="theUserInfoDiv">
-          <UserInfoDiv />
+          <div id="inner-theUserInfoDiv">
+            <div className="userInfoShadowDivTop"></div>
+            <div id="userInfo-background1">
+              <div id="userInfo-background2">
+                <div id="userInfo-background3">
+                  <div id="userInfo-background4">
+                    <div id="userInfo-background5">
+                      <UserInfoDiv />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="userInfoShadowDiv"></div>
         </div>
       </div>
       <div id="rightsideDiv">
-      <div id="CurrentGameDiv">
-        <CurrentGameDiv />
-      </div>
+        <div id="CurrentGameDiv">
+          <div id="inner-CurrentGameDiv">
+          <div className="currentGameShadowDivTop"></div>
+            <CurrentGameDiv />
+          </div>
+          <div className="currentGameShadowDiv"></div>
+        </div>
       </div>
     </div>
   );
