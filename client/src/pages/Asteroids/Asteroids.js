8a2ab28f82e688c316858cc17a1f265ad4100941
Michael Sanderson <jawm17@gmail.com>
2021-06-17 16:03:44 -0700

    game scores

--- client/src/pages/Asteroids/Asteroids.js
+++ client/src/pages/Asteroids/Asteroids.js
@@ -12,7 +12,7 @@ export default function Asteroids(props) {
     const GAME_LIVES = 3; // starting number of lives
     const LASER_DIST = 0.6; // max distance laser can travel as fraction of screen width
     const LASER_EXPLODE_DUR = 0.1; // duration of the lasers' explosion in seconds
-    const LASER_MAX = 10; // maximum number of lasers on screen at once
+    const LASER_MAX = 20; // maximum number of lasers on screen at once
     const LASER_SPD = 500; // speed of lasers in pixels per second
     const ROID_JAG = 0.4; // jaggedness of the asteroids (0 = none, 1 = lots)
     const ROID_PTS_LGE = 20; // points scored for a large asteroid
@@ -20,13 +20,13 @@ export default function Asteroids(props) {
     const ROID_PTS_SML = 100; // points scored for a small asteroid
     const ROID_NUM = 3; // starting number of asteroids
     const ROID_SIZE = 100; // starting size of asteroids in pixels
-    const ROID_SPD = 50; // max starting speed of asteroids in pixels per second
+    const ROID_SPD = 120; // max starting speed of asteroids in pixels per second
     const ROID_VERT = 10; // average number of vertices on each asteroid
     const SHIP_BLINK_DUR = 0.1; // duration in seconds of a single blink during ship's invisibility
     const SHIP_EXPLODE_DUR = 0.3; // duration of the ship's explosion in seconds
     const SHIP_INV_DUR = 3; // duration of the ship's invisibility in seconds
     const SHIP_SIZE = 30; // ship height in pixels
-    const SHIP_THRUST = 5; // acceleration of the ship in pixels per second per second
+    const SHIP_THRUST = 15; // acceleration of the ship in pixels per second per second
     const SHIP_TURN_SPD = 360; // turn speed in degrees per second
     const SHOW_BOUNDING = false; // show or hide collision bounding
     const SHOW_CENTRE_DOT = false; // show or hide ship's centre dot
--- client/src/pages/Snake/SnakeGame.js
+++ client/src/pages/Snake/SnakeGame.js
@@ -146,7 +146,7 @@ export default function SnakeGame() {
     setSnake(SNAKE_START);
     setApple(APPLE_START);
     setDir([0, -1]);
-    setSpeed(SPEED);
+    setSpeed(90);
     setStartDisplay("none");
     setEndDisplay("none");
     setGameOver(false);
@@ -305,17 +305,17 @@ export default function SnakeGame() {
               height={`${CANVAS_SIZE[1]}px`}
             />
           </div>
-          {/* <div id="snakeInfo">
+          <div id="snakeInfo">
             <div className="snakeInfoEl">
-              Highscore: {scores[0]?.score || 0}
+            Score: {score}
             </div>
             <div className="snakeInfoEl">
               Jackpot: {parseFloat(pot.toFixed(6))} ETH
               </div>
             <div className="snakeInfoEl">
-              Personal Best: {personalBest}
+            Highscore: {scores[0]?.score || 0}
             </div>
-          </div> */}
+          </div>
           {/* <div id="snakeControlsOuter">
             <div id="snakeControls">
               <img className="snakeControlBtn" onClick={() => setDir(DIRECTIONS[37])} src="https://firebasestorage.googleapis.com/v0/b/gamesresources-28440.appspot.com/o/back-button.png?alt=media&token=f61923a9-ca19-4aaf-974f-31c5f2f2c632" alt="left button"></img>
